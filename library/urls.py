d0822a0cd49bf32a425b277ba7de1b258ccf1eb7
Abbas Jannati <zaidisyed113@gmail.com>
2026-05-22 17:51:49 +0530

    Added REST API

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/books/', views.book_list, name='book_list'),
+    
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Book
-
+from .serializers import BookSerializer
 def home(request):
     books = Book.objects.all()
     context = {
@@ -11,6 +12,13 @@ def home(request):
 
     }
     return render(request, 'home.html',context)
+
+# API view to get all books
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
 
 # Create your views here.
