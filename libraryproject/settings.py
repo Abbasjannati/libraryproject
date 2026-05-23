02e29c86c150f23b036ff684120cd69230172640
Abbas Jannati <zaidisyed113@gmail.com>
2026-05-24 00:31:05 +0530

    ADD LOGIN PAGE

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'library',
     'rest_framework',
 ]
+LOGIN_REDIRECT_URL = '/'  
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
