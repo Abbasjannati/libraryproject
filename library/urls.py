9ebf2ce58adcdd16bbb30a803d6d286907ac32a1
Abbas Jannati <zaidisyed113@gmail.com>
2026-05-24 00:55:11 +0530

    NEW BOOK ADD

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/books/', views.book_list, name='book_list'),
-    
+    path('add-book/', views.add_book, name='add_book'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render, redirect
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
+from .forms import BookForm
 def home(request):
     search = request.GET.get('search', '')
 
@@ -10,6 +12,8 @@ def home(request):
         books = Book.objects.filter(title__icontains=search)
     else:
         books = Book.objects.all()
+
+    
     context = {
         'name': 'Abbas zaidi',
         'books': books
@@ -17,6 +21,19 @@ def home(request):
 
     }
     return render(request, 'home.html',context)
+
+def add_book(request):
+        if request.method == 'POST':
+            form =BookForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        else:
+            form = BookForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'add_book.html', context)
 
 # API view to get all books
 @api_view(['GET'])
